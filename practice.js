884b8f4d6644f92ba78fc2f96bfb8d79180c5320
Your Name <Meesh@MacBook-Pro.local>
2018-01-27 18:16:59 -0700

    done

--- practice.js
+++ practice.js
@@ -18,6 +18,10 @@ var arr = [10,20,30];
 */
 
 //Code Here
+function first(arr){
+  return arr[0];
+}
+console.log(first(arr));
 
 
 
@@ -33,7 +37,10 @@ var arr = [40,50,60];
 */
 
 //Code Here
-
+function last(arr){
+  return arr[arr.length-1];
+}
+console.log(last(arr));
 
 
 ////////// PROBLEM 3 //////////
@@ -48,8 +55,12 @@ var family = ['Tyler', 'Jordyn', 'Ryan', 'Chelsey', 'Ireland'];
 */
 
 //Code Here
-
-
+function looper(family){
+  for(let i=0;i<family.length;i++){
+    alert(family[i]);
+  }
+}
+console.log(looper(family));
 
 ////////// PROBLEM 4 //////////
 
@@ -63,6 +74,13 @@ var letters = ['A', 'B', 'C', 'D', 'E'];
 */
 
 //Code Here
+function reversedLooper(letters){
+  for(let i=letters.length-1;i>=0;i--){
+    alert(letters[i]);
+  }
+}
+console.log(reversedLooper(letters));
+
 
 
 
@@ -78,6 +96,16 @@ var nums = [1,2,3,6,22,98,45,23,22,12];
 */
 
 //Code Here
+function evenFinder(arr){
+  let arr1=[];
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]%2===0){
+      arr1.push(arr[i]);
+    }
+  }
+  return arr1;
+}
+console.log(evenFinder(nums));
 
 
 
